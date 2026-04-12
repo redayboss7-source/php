c9871ba3ea31ea97d209ada601703ecd4f5e7532
redayboss7 <redayboss7@gmail.com>
2026-04-12 12:23:17 +0600

    prime-number

--- oop/prime number.php/prime.php
+++ oop/prime number.php/prime.php
@@ -1,18 +1,42 @@
-<?php
-function isPrime($num) {
-    if ($num <= 1) {
-        return false;
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find Prime Number</title>
+</head>
+<body>
+    <form method="post">
+    Enter Number: 
+    <input type="number" name="num" id="num" required>
+    <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+    if (isset($_POST['submit'])) { 
+        
+        $num = $_POST['num'];
+        $isPrime = true;
 
-    for ($i = 2; $i <= sqrt($num); $i++) {
-        if ($num % $i == 0) {
-            return false;
+        if ($num == 1|| $num ==0) {
+           echo "<h3> $num is NOT a Prime Number or Composite</h3>";
+           return;
+        } else {
+            for ($i = 2; $i < ($num); $i++) {
+                if (($num % $i) == 0) {
+                    $isPrime = false;
+                    break;    
+
+                }
+            }
+        }
+
+        if ($isPrime) {
+            echo "<h3> $num is a Prime Number</h3>";
+        } else{
+            echo "<h3> $num is NOT a Prime Number </h3>";
         }
     }
-
-    return true;
-}
-
-
-
-?>
+    
+    ?>
+</body>
+</html>
